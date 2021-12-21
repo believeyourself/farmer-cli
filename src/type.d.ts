40ed5feb9f5baeebe37f4a7dd462978c03f7cc62
HeZhihao <ylwshzh@163.com>
2021-12-21 19:53:09 +0800

    feat: mysql pool

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,10 +1,11 @@
 /*
  * @Date: 2021-12-01 10:14:35
  * @LastEditors: lzj
- * @LastEditTime: 2021-12-01 10:15:40
+ * @LastEditTime: 2021-12-21 19:42:30
  * @FilePath: \farmer-cli\src\type.d.ts
  */
 type YArgs = {
   _: string[],
   [key:string]:any
 }
+declare module "mysql-await";
